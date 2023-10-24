c2b90ad203a0dffd0bad2a0b895223dcbd0b87f7
LUIS ALEXIS DE FREITAS <131389113+LAFG16@users.noreply.github.com>
2023-10-24 14:42:55 +0000

    First Commit

--- src/main.py
+++ src/main.py
@@ -1,2 +1,34 @@
 # Obligatorio: Generar una tabla usando Python con TODOS los ficheros (recursivamente) del workspace que contenga el nombre del fichero, el peso REAL y la última fecha de modificación.
 # Opcional: Hacer lo mismo que en la línea anterior pero en Bash Scripting y exportando un CSV
+import os
+import time
+import pandas as pd
+
+#actual rute
+dir = os.getcwd()
+
+#files list
+
+files = os.listdir(dir)
+
+#function size
+
+def get_size(x):
+    return os.path.getsize(x)
+
+#function time
+
+def get_time(x):
+    return time.ctime(os.path.getmtime(x))
+
+#size files list
+size = [get_size(file) for file in files]
+
+#last modification files list
+time_ = [get_time(file) for file in files]
+
+#covert lists in DataFrame
+data_dir = pd.DataFrame({'files': files,'size': size,'time': time_})
+
+#generate csv
+data_dir.to_csv('table_files.csv', index=False)
--- src/main.sh
+++ src/main.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-
+ls -la | awk '{print $9, $5, $6" "$7} > table_files_bash.csv
